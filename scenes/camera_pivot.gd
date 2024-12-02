69de3fb74017aaad3b629e64df5fde223ebf9a98
Daniel Justus <daniel.justus32@gmail.com>
2024-12-01 16:09:01 -0800

    Camera rotation/zoom in place

--- scenes/camera_pivot.gd
+++ scenes/camera_pivot.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @onready var player: CharacterBody3D = get_tree().get_nodes_in_group("PlayerCharacter")[0]
+@onready var sky_camera: Camera3D = $SkyCamera
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,4 +9,21 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	# Camera will always track the position of player, keepin them at center screen
 	global_position = player.global_position
+	
+	# Handle camera rotation
+	if Input.is_action_pressed("rotate_camera_left"):
+		rotation_degrees.y -= 1
+	if Input.is_action_pressed("rotate_camera_right"):
+		rotation_degrees.y += 1
+	
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("zoom_in"):
+		if sky_camera.size > 5:
+			sky_camera.size -= 1
+			
+	if Input.is_action_just_pressed("zoom_out"):
+		if sky_camera.size < 100:
+			sky_camera.size += 1
+			
